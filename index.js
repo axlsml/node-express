f54f220414c0c181fd3a3e4f99402d4031e8539a
Axel Bock <axel@bockig.com>
2017-10-28 12:54:59 +0200

    express rest ohne router crap

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,6 +9,43 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all dem dishes <3. wala');
+});
+app.post('/dishes', (req, res, next) => {
+    res.end(`adding ${req.body.name} / ${req.body.description}`);
+});
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`put not supported`);
+});
+app.delete('/dishes', (req, res, next) => {
+    res.end('deleting all dem dishes :(');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send ${req.params.dishId} <3. wala');
+});
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`post not supported`);
+});
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write(`put ${req.params.dishId}\n`);
+    res.end(`put ${req.params.dishId}`);
+});
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end(`del ${req.params.dishId}`);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
